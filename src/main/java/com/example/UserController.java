2c25acc4ccb55ee88de4e3bd82c1a7a280a026c3
Anshul Singhal <anshulsinghal612@gmail.com>
2021-04-23 20:26:02 +0530

    Support for Cross origin issue

--- src/main/java/com/example/UserController.java
+++ src/main/java/com/example/UserController.java
@@ -1,7 +1,7 @@
 package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class UserController
 {
     @Autowired
